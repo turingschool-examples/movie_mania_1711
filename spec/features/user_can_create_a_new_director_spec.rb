e2767a66b8c8263d132cfb8acae38284f7bbd167
Ali Schlereth <aschlere@sbcglobal.net>
2018-02-05 09:22:42 -0700

    Add new director test

---
+++ spec/features/user_can_create_a_new_director_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe "as a user" do
+  describe "when I visit new" do
+    it "they can create a director" do
+      visit "/directors/new"
+
+      fill_in "director[name]", with: "Copola"
+
+      expect(page).to have_content("Director: Copola")
+    end
+  end
+end
