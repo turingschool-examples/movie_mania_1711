96b724d088c14422fd356dddde313dda2ebd4bb1
Ilana Corson <ilana@turing.io>
2018-02-09 16:41:38 -0700

    add ass

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+CSV.foreach("./data/movies.csv", headers: true) do |row|
+  director = Director.find_or_create_by(name: row[4])
+  movie = Movie.create(
+                title: row[1],
+                description: row[3],
+                director: director)
+                puts "Created #{movie.title}"
+end
