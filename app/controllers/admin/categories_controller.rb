05e69f3023e9d15c44e7e82851507cc579cc5430
Ali Schlereth <aschlere@sbcglobal.net>
2018-02-13 10:12:31 -0700

    Add require admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,14 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :require_admin
 
   def index
 
   end
+
+  private
+
+    def require_admin
+      render file: "/public/404" unless current_admin?
+    end
 
 end
