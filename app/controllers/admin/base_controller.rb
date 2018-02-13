7454668fc32ae5cc77b93ee3dc74f343fdc4f560
Ali Schlereth <aschlere@sbcglobal.net>
2018-02-13 11:59:11 -0700

    Add admin base controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,7 +2,6 @@ class Admin::BaseController < ApplicationController
   before_action :require_admin
 
   private
-
     def require_admin
       render file: "/public/404" unless current_admin?
     end
