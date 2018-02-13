c8ac317b7719d81bc531fe84c72106a8b21b0a30
Ilana Corson <ilana@turing.io>
2018-02-13 08:52:15 -0700

    add auth lesson

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :username)
+  end
+end
