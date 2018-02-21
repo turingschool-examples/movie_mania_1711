c262b9973ef160cb3ded278d94e625c4fe3ff1fe
Ali Schlereth <aschlere@sbcglobal.net>
2018-02-21 10:02:18 -0700

    Update carts controller to use session[:cart]

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,13 @@
 class CartsController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
 
   def create
     movie = Movie.find(params[:movie_id])
-    flash[:notice] = "You now have 1 #{movie.title} in your cart."
+    session[:cart] ||= Hash.new(0)
+    session[:cart][movie.id.to_s] = session[:cart][movie.id.to_s] + 1
+    # flash[:notice] = "You now have #{session[:cart][movie.id.to_s]} #{movie.title} in your cart."
+    flash[:notice] = "You now have #{pluralize(session[:cart][movie.id.to_s], movie.title)} in your cart."
     redirect_to director_movies_path(params[:director_id])
   end
 
