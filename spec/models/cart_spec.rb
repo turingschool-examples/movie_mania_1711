b6009b4639151ade9ab194cf303a7e243ba3da54
Ali Schlereth <aschlere@sbcglobal.net>
2018-02-21 10:52:08 -0700

    Add count_of on Cart spec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -22,9 +22,23 @@ describe Cart, type: :model do
 
     it "handles default nil" do
       cart = Cart.new(nil)
-      
+
       cart.add_movie("1")
       expect(cart.contents).to eq("1" => 1)
+    end
+  end
+
+  describe "#count_of" do
+    it "returns a count of a particular movie" do
+      cart = Cart.new({"1" => 2, "2" => 3})
+
+      expect(cart.count_of("2")).to eq(3)
+    end
+
+    it "returns 0 when movie id is not present key" do
+      cart = Cart.new({"1" => 2, "2" => 3})
+
+      expect(cart.count_of("5")).to eq(0)
     end
   end
 end
