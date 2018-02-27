a1d32d0a9ee6466846ffe84008243fd5c74f03d6
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-27 09:38:48 -0700

    Adds spec for unregistered user visiting awards index

---
+++ spec/features/awards/unregistered_user_can_visit_awards_index_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "As an unregistered user" do
+  before(:each) do
+    @award_1 = Award.create!(title: "Best Picture")
+    @award_2 = Award.create!(title: "Best Actress")
+  end
+  describe "I visit the awards index" do
+    it "I can see a list of award links" do
+      visit awards_path
+
+      expect(page).to have_link(@award_1.name)
+      expect(page).to have_link(@award_2.name)
+
+      click_on(@award_1.name)
+
+      expect(current_path).to eq(award_path(@award_1))
+    end
+
+    it "I do not see the form to create a new award" do
+      visit awards_path
+
+      expect(page).to_not have_content("New Award:")
+      expect(page).to_not have_content("Submit")
+    end
+  end
+end
