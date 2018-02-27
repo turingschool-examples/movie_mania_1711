8047f7073aa02b6724429f418e0d5dc1f36c0818
kylesallette <kylesallette@gmail.com>
2018-02-27 10:08:34 -0700

    second test passing

--- spec/features/admin_see_form_to_create_new_awards_and_then_sees_all_the_awards_spec.rb
+++ spec/features/admin_see_form_to_create_new_awards_and_then_sees_all_the_awards_spec.rb
@@ -16,6 +16,28 @@ require 'rails_helper'
         expect(page).to have_content("best actor")
 
 
+
+
+     end
+   end
+
+   describe 'user does not see link to create a new award'  do
+     it 'they see list of all awards' do
+       user = User.create(username: "kyle", password: "password")
+       movie_1 = Movie.create(title:"something good", description:"some eoetingd")
+       movie_2 = Movie.create(title:"something else", description:"a movie about a movie")
+       award_1 = Award.create(title: "best picture", movie: movie_1)
+       award_2 = Award.create(title: "best actor", movie: movie_2 )
+       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+       visit  awards_path
+
+        expect(page).to_not have_content("Create new award")
+        expect(page).to have_content("Awards")
+        expect(page).to have_content("best picture")
+        expect(page).to have_content("best actor")
+
+
        save_and_open_page
 
      end
