3180793da60957a6e9f578d19d5fed5b20a8e1ad
Anna Royer <anna.royer@colorado.edu>
2018-02-27 09:15:41 -0700

    add routes for admin awards

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :categories, only: [:index]
+    resources :awards, only: [:index, :create]
   end
 
   resources :carts, only: [:create]
