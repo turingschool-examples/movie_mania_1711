4650c69244a1bb0bf632b95c7512abbc6c38d77a
Luke Chambers <lukenatchambers@gmail.com>
2018-02-27 10:39:58 -0700

    Minor refactors, make sure routes are all needed

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :awards
-
+  resources :awards, only: [:index, :create, :show]
 end
