9ac4d2a05552c6c562727720c9f76b8a99542820
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-27 09:32:07 -0700

    Adds ability for admin to create new award

---
+++ app/controllers/awards_controller.rb
@@ -0,0 +1,17 @@
+class AwardsController < ApplicationController
+  def index
+    @awards = Award.all
+    @award = Award.new()
+  end
+
+  def create
+    @award.new(award_params)
+    @award.save
+    redirect_to awards_path
+  end
+
+  private
+    def award_params
+      params.require(:award).permit(:title)
+    end
+end
