9d075848008e652297c37d4c8a9e28df505ed0f5
amj133 <amj@vt.edu>
2018-02-27 10:34:32 -0700

    visitor sees movies on award show passing

--- spec/features/awards/visitor_sees_movies_on_awards_show_spec.rb
+++ spec/features/awards/visitor_sees_movies_on_awards_show_spec.rb
@@ -2,23 +2,25 @@ require 'rails_helper'
 
 describe "visitor sees movies on awards show" do
   context "visits awards index and clicks on award" do
-    it "brough to award show and movies that won are shown" do
+    it "brough to award show and movies that won are shown with year" do
       director = Director.create!(name: "Joel Coen")
       fargo = director.movies.create!(title: "Fargo",
                                       description: "ransom gone wrong")
-      award = movie.awards.create!(name: "Best Picture")
-      AwardMovie.find(1).update(year: 1996)
+      award = Award.create!(name: "Best Picture")
+      AwardMovie.create!(award_id: award.id, movie_id: fargo.id, year: 1996)
       director = Director.create!(name: "Steven Spieldberg")
       gump = director.movies.create!(title: "Forrest Gump",
                                      description: "20th century history through the eyes of an unsuspecting individual")
-      AwardMovie.find(2).update(year: 1994)
+      AwardMovie.create!(award_id: award.id, movie_id: gump.id, year: 1994)
 
       visit awards_path
       click_on("Best Picture")
 
       expect(current_path).to eq(award_path(award))
-      expect(page).to have_content("#{fargo.title}, Year: 1996")
-      expect(page).to have_content("#{gump.title}, Year: 1994")
+      expect(page).to have_content("Fargo")
+      expect(page).to have_content("Year Won: 1996")
+      expect(page).to have_content("Forrest Gump")
+      expect(page).to have_content("Year Won: 1994")
     end
   end
 end
