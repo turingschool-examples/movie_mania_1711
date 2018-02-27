1ebda24b8257eeba4663e541be05d74bdc895cbe
Andrew Dymond <andy.dymond@gmail.com>
2018-02-27 09:16:40 -0700

    Modify test in admin_can_create_awards_spec.rb

--- spec/features/admin_can_create_awards_spec.rb
+++ spec/features/admin_can_create_awards_spec.rb
@@ -8,7 +8,7 @@ require 'rails_helper'
 # I see a list of awards I have created.
 
 describe "create award" do
-  it "allows user logged in as admin to create a new award" do
+  it "allows user logged in as admin to create a new award from awards index" do
     admin = create(:user, role: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
     visit awards_path
@@ -16,7 +16,7 @@ describe "create award" do
     fill_in "Title", with: "Best Tomato"
     click_on "Create Award"
 
-    expect(current_path).to eq(awards_path)
+    expect(current_path).to eq(user_path(admin))
     expect(page).to have_content("Best Tomato")
   end
 end
