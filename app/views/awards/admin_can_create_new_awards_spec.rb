a3b42867c46bcf8b4269b0d502e48e62f87a9d15
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-27 09:09:58 -0700

    Adds feature spec for user_story_1

---
+++ app/views/awards/admin_can_create_new_awards_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "As an admin" do
+  describe "When I visit awards index" do
+    it "I can create an award" do
+      admin = User.create!(username: 'memcmahon', password: 'test', role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+      visit awards_path
+
+      fill_in('award[title]', with => "Best Picture")
+      click_on('submit')
+
+      expect(page).to have_content("Awards: Best Picture")
+    end
+  end
+end
