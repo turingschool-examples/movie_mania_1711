f8a6b15db599c3e2fd347c0cfae286fd418dc6bb
Andrew Dymond <andy.dymond@gmail.com>
2018-02-27 09:15:39 -0700

    Add admin_can_create_awards_spec.rb

---
+++ spec/features/admin_can_create_awards_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+# As an admin user,
+# When I visit the awards index,
+# I can see a form to create new awards,
+# When I fill out the form with the awards title,
+# And click submit,
+# I see a list of awards I have created.
+
+describe "create award" do
+  it "allows user logged in as admin to create a new award" do
+    admin = create(:user, role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+    visit awards_path
+
+    fill_in "Title", with: "Best Tomato"
+    click_on "Create Award"
+
+    expect(current_path).to eq(awards_path)
+    expect(page).to have_content("Best Tomato")
+  end
+end
