bac50bf09ac7a49828537c83b0a9f0c75b42e5e2
amj133 <amj@vt.edu>
2018-02-27 10:10:53 -0700

    visitor can see awards on movie show but not year won yet

--- spec/features/movies/visitor_can_access_movie_show_from_movie_index_spec.rb
+++ spec/features/movies/visitor_can_access_movie_show_from_movie_index_spec.rb
@@ -13,6 +13,23 @@ describe "visitor can access movie show" do
       expect(current_path).to eq(movie_path(movie))
       expect(page).to have_content(movie.title)
     end
+
+    it "show displays award names and year won" do
+      director = Director.create!(name: "Joel Coen")
+      movie = director.movies.create!(title: "Fargo",
+                                      description: "ransom gone wrong")
+      award_1 = movie.awards.create!(name: "Best Picture")
+      award_2 = movie.awards.create!(name: "Best Sound")
+      award_3 = movie.awards.create!(name: "Best Action Film")
+
+      visit director_movies_path(director)
+      click_on("Fargo")
+
+      expect(page).to have_content("Awards won:")
+      expect(page).to have_content("Best Picture")
+      expect(page).to have_content("Best Sound")
+      expect(page).to have_content("Best Action Film")
+    end
   end
 end
 
