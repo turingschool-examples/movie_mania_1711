e55fef7e1c3dc33c90d5e218ec948aa694d509d4
Andrew Dymond <andy.dymond@gmail.com>
2018-02-27 10:00:04 -0700

    Create MovieAwards table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227163416) do
+ActiveRecord::Schema.define(version: 20180227165916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20180227163416) do
     t.string "name"
   end
 
+  create_table "movie_awards", force: :cascade do |t|
+    t.bigint "movie_id"
+    t.bigint "award_id"
+    t.integer "year"
+    t.index ["award_id"], name: "index_movie_awards_on_award_id"
+    t.index ["movie_id"], name: "index_movie_awards_on_movie_id"
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -53,5 +61,7 @@ ActiveRecord::Schema.define(version: 20180227163416) do
   add_foreign_key "actor_movies", "actors"
   add_foreign_key "actor_movies", "movies"
   add_foreign_key "awards", "users"
+  add_foreign_key "movie_awards", "awards"
+  add_foreign_key "movie_awards", "movies"
   add_foreign_key "movies", "directors"
 end
