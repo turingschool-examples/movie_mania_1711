1f2ae85aa7f8d73390892e77f472675f2f6d9357
Andrew Dymond <andy.dymond@gmail.com>
2018-02-27 09:26:21 -0700

    Add awards table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221182641) do
+ActiveRecord::Schema.define(version: 20180227162505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 20180221182641) do
 
   create_table "actors", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "awards", force: :cascade do |t|
+    t.string "title"
   end
 
   create_table "directors", force: :cascade do |t|
