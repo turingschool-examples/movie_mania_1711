a8b0cfd4dc172aeb6effb9d3e6741f40234b433b
Andrew Dymond <andy.dymond@gmail.com>
2018-02-27 09:18:14 -0700

    Add admin_awards_path to config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :categories, only: [:index]
+    resources :awards, only: [:index]
   end
 
   resources :carts, only: [:create]
--- spec/features/admin_can_create_awards_spec.rb
+++ spec/features/admin_can_create_awards_spec.rb
@@ -11,7 +11,7 @@ describe "create award" do
   it "allows user logged in as admin to create a new award from awards index" do
     admin = create(:user, role: 1)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-    visit awards_path
+    visit admin_awards_path
 
     fill_in "Title", with: "Best Tomato"
     click_on "Create Award"
