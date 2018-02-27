a983935ce72d5d2918ccc57c5370399276d45baf
Andrew Dymond <andy.dymond@gmail.com>
2018-02-27 09:45:59 -0700

    Fix typo in visitor_sees_awards_index_spec.rb

--- spec/features/visitor_sees_awards_index_spec.rb
+++ spec/features/visitor_sees_awards_index_spec.rb
@@ -1,12 +1,14 @@
 require "rails_helper"
 
 describe "visitor sees awards index" do
-  user = create(:user)
-  award_1 = user.awards.create(title: "Best Grapefruit")
-  award_2 = user.awards.create(title: "Best Cucumber")
+  it "displays list of awards" do
+    user = create(:user)
+    award_1 = user.awards.create(title: "Best Grapefruit")
+    award_2 = user.awards.create(title: "Best Cucumber")
 
-  visit awards_path
+    visit awards_path
 
-  expect(page).to have_content(award_1.title)
-  expect(page).to have_content(award_2.title)
+    expect(page).to have_content(award_1.title)
+    expect(page).to have_content(award_2.title)
+  end
 end
