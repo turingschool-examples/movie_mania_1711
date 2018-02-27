503930432e9ff4690502e45b5b4f3fa48e5ec905
Luke Chambers <lukenatchambers@gmail.com>
2018-02-27 10:32:53 -0700

    Finish building test suite

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -6,4 +6,15 @@ describe Movie do
   it { should have_many :oscars } #haha
   it { should have_many(:actors).through :actor_movies }
   it { should have_many(:awards).through :oscars }
+
+  describe ".year" do
+    it "should return a year probably" do
+      admin = create(:admin)
+      award = create(:award, user: admin)
+      movie = create(:movie)
+      Oscar.create(movie: movie, award: award, year: 10212)
+
+      expect(movie.year(award)).to eq(10212)
+    end
+  end
 end
