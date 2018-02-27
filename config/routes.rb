10195c70b34fdaaa5e3c5f39ddb6fc8dd4c31ece
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-27 09:22:08 -0700

    Adds routes for award index and create

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show]
 
+  resources :awards, only: [:index, :create]
+
   namespace :admin do
     resources :categories, only: [:index]
   end
