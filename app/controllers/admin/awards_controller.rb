78afbdfc87c3be4bef953c1ac4584d42e3f74dc2
kylesallette <kylesallette@gmail.com>
2018-02-27 10:18:01 -0700

    comming along

--- app/controllers/admin/awards_controller.rb
+++ app/controllers/admin/awards_controller.rb
@@ -1,7 +1,27 @@
 class Admin::AwardsController < Admin::BaseController
 
+  def new
+    @award = Award.new
+  end
+
   def index
     @awards = Award.all
+  end
+
+  def create
+    @award = Award.new(award_params)
+    if @award.save
+      redirect_to admin_awards_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def award_params
+    params.require(:award).permit(:title)
   end
 
 end
--- spec/features/admin_see_form_to_create_new_awards_and_then_sees_all_the_awards_spec.rb
+++ spec/features/admin_see_form_to_create_new_awards_and_then_sees_all_the_awards_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-   describe 'admin sees award index and form to create a new award'  do
+   describe 'admin sees award index and link to create a new award'  do
      it 'after creating award they click submit and sees all awards' do
        user = User.create(username: "kyle", password: "password", role: 1)
        movie_1 = Movie.create(title:"something good", description:"some eoetingd")
@@ -14,8 +14,6 @@ require 'rails_helper'
        expect(page).to have_content("Create new award")
         expect(page).to have_content("best picture")
         expect(page).to have_content("best actor")
-
-
 
 
      end
@@ -38,7 +36,33 @@ require 'rails_helper'
         expect(page).to have_content("best actor")
 
 
-       save_and_open_page
+
 
      end
    end
+
+
+      describe 'admin sees award index and link to create a new award'  do
+        it 'after creating award they click submit and sees all awards' do
+          user = User.create(username: "kyle", password: "password", role: 1)
+          movie_1 = Movie.create(title:"something good", description:"some eoetingd")
+          movie_2 = Movie.create(title:"something else", description:"a movie about a movie")
+          award_1 = Award.create(title: "best picture", movie: movie_1)
+          award_2 = Award.create(title: "best actor", movie: movie_2 )
+          allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+          visit admin_awards_path
+
+          click_link("Create new award")
+
+          expect(page).to have_content("Title")
+          expect(page).to have_button("Create Award")
+
+          fill_in("Title", with:"best of something")
+
+          click_button("Create Award")
+
+
+
+        end
+      end
